97bc467722ccbaec4abaa931ad771fd247e3cee5
bernat <elbernat@gmail.com>
2020-04-04 20:12:33 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,101 @@
-console.log('1.js-connected');
+console.log('mainjs charged')
 
-//////////////// CONTROL PARAMS ///////////
-let PARAMS = {
-	// PARAMS
-};
-//////////////// CONTROL PARAMS ///////////
+function make2DArray(rows, cols) {
+	var arr = new Array(rows); //like arr[]; but with number of columns hardcoded
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = new Array(cols);
+	}
+	return arr;
+}
 
-//////////////// TWEAKPANE ////////////////
-//////////////// TWEAKPANE ////////////////
+let angle = 0;
+let w = 120;
+let cols;
+let rows;
+let curves;
 
-//////////////// MICROBIT PAIRING /////////
-//////////////// MICROBIT PAIRING /////////
+function setup() {
+	// Adding the canvas to a specific div
+	let myCanvas = createCanvas(windowWidth, windowHeight);
+	myCanvas.parent('#theCanvas');
+	console.log('canvas created');
 
-//////////////// CANVAS ///////////////////
+	cols = floor(width / w) - 1;
+	rows = floor(height / w) - 1;
+	curves = make2DArray(rows, cols);
 
-//////////////// CANVAS ///////////////////
+	for (let j = 0; j < rows; j++) {
+		for (let i = 0; i < cols; i++) {
+			curves[j][i] = new Curve();
+		}
+	}
+}
 
-//////////////// SLIDE-PANEL //////////////
-//////////////// SLIDE-PANEL //////////////
+function draw() {
+	background(10);
+	let d = w - 0.2 * w;
+	let r = d / 2;
+
+	noFill();
+	stroke(255);
+	for (let i = 0; i < cols; i++) {
+		let cx = w + i * w + w / 2;
+		let cy = w / 2;
+		strokeWeight(1);
+		stroke(255);
+		ellipse(cx, cy, d, d);
+		let x = r * cos(angle * (i + 1) - HALF_PI);
+		let y = r * sin(angle * (i + 1) - HALF_PI);
+		strokeWeight(8);
+		stroke(255);
+		point(cx + x, cy + y);
+		stroke(255, 150);
+		strokeWeight(1);
+		line(cx + x, 0, cx + x, height);
+
+		for (let j = 0; j < rows; j++) {
+			curves[j][i].setX(cx + x);
+		}
+	}
+
+	noFill();
+	stroke(255);
+	for (let j = 0; j < rows; j++) {
+		let cx = w / 2;
+		let cy = w + j * w + w / 2;
+		strokeWeight(1);
+		stroke(255);
+		ellipse(cx, cy, d, d);
+		let x = r * cos(angle * (j + 1) - HALF_PI);
+		let y = r * sin(angle * (j + 1) - HALF_PI);
+		strokeWeight(8);
+		stroke(255);
+		point(cx + x, cy + y);
+		stroke(255, 150);
+		strokeWeight(1);
+		line(0, cy + y, width, cy + y);
+
+		for (let i = 0; i < cols; i++) {
+			curves[j][i].setY(cy + y);
+		}
+	}
+
+	for (let j = 0; j < rows; j++) {
+		for (let i = 0; i < cols; i++) {
+			curves[j][i].addPoint();
+			curves[j][i].show();
+		}
+	}
+
+	angle -= 0.01;
+
+	if (angle < -TWO_PI) {
+		for (let j = 0; j < rows; j++) {
+			for (let i = 0; i < cols; i++) {
+				curves[j][i].reset();
+			}
+		}
+		// saveFrame("lissajous#####.png");
+		angle = 0;
+	}
+}
--- js/music.js
+++ js/music.js
@@ -1,16 +1,1 @@
-window.onload = function() {
-
-    let canvas = document.querySelector('#layer-2');
-    console.log(canvas);
-
-}
-
-let r = new Rune({
-    container: "#layer-2",
-    width: 500,
-    height: 400
-  });
-  
-  r.rect(0, 0, 200, 200).fill(0, 0, 255);
-  
-  r.draw();
+console.log('musicjs charged')
